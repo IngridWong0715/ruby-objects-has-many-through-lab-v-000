48064b0dc813f3d729eb489f69763dd78ec91f2f
Ingrid Wong <yw340@georgetown.edu>
2018-01-20 16:43:42 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -4,4 +4,17 @@ class Patient
     @name = name
     @appointments = []
   end
+
+  def add_appointment(appointment)
+    @appointments << appointment
+    appointment.patient = self
+  end
+
+  def appointments
+    @appointments
+  end
+
+  def doctors
+    self.appointments.collect {|appointment| appointment.doctor}
+  end
 end
