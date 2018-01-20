7e80a55c2710719a432e35c9c5b57b49dfcb02ec
Ingrid Wong <yw340@georgetown.edu>
2018-01-20 16:46:04 +0000

    automatically backed up by learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,7 +1,7 @@
 class Appointment
 
-  attr_accessor :patient
-  attr_reader :date, :doctor
+  attr_accessor :patient, :doctor
+  attr_reader :date
 
   def initialize(date, doctor)
     @date = date
